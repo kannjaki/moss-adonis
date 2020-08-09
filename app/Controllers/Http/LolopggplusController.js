32bfd2e1c161975c7eccecdd8b3ccc3a10682a57
kannjaki <kannzaki0980@gmail.com>
2020-08-09 15:30:00 +0900

    Update LolopggplusController.js

--- app/Controllers/Http/LolopggplusController.js
+++ app/Controllers/Http/LolopggplusController.js
@@ -4,7 +4,7 @@ var sl = require('await-sleep');
 var ladders = new Map();
 var count = 0;
 var headers = {
-  "X-Riot-Token": "RGAPI-e54eaa0c-3d7d-4d0b-b8f6-5cd99662fe7e"
+  "X-Riot-Token": "RGAPI-5f069562-3746-485b-8b4b-92e45209e6a0"
 }
 class LolopggplusController {
   async showLadder({
